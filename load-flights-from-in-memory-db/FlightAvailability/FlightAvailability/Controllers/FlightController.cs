1dfe3684dec967ba30903c87a5c2998815bbd544
Marcial Rosales <mrosales@pivotal.io>
2017-08-30 12:02:53 +0200

    create new project from skeleton and add a simple controller that returns mock flights

---
+++ load-flights-from-in-memory-db/FlightAvailability/FlightAvailability/Controllers/FlightController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+using FlightAvailability.Models;
+using System;
+
+namespace FlightAvailability.Controllers
+{
+
+    [Route("api")]
+    public class FlightController : ApiController
+    {
+
+        public FlightController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<List<Flight>> find([FromUri, Required] string origin, [FromUri, Required] string destination)
+        {
+            System.Diagnostics.Debug.WriteLine($"Find {origin}/{destination}");
+            return InitialFlights();
+
+
+        }
+        private List<Flight> InitialFlights()
+        {
+            var flights = new List<Flight>
+            {
+                new Flight{Origin="MAD",Destination="GTW"},
+                new Flight{Origin="MAD",Destination="FRA"},
+                new Flight{Origin="MAD",Destination="LHR"},
+                new Flight{Origin="MAD",Destination="ACE"}
+            };
+            return flights;
+        }
+    }
+
+
+}
---
+++ load-flights-from-in-memory-db/FlightAvailability/FlightAvailability/Models/Flight.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace FlightAvailability.Models
+{
+    public class Flight
+    {
+        public long Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Date { get; set;  }
+        public string Code { get; set;  }
+
+        public override string ToString()
+        {
+            return $"Flight[{Id} {Origin}/{Destination}]";
+        }
+    }
+
+}
