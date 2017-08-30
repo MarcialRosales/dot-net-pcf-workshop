0f6961b3ac2a82f33be0b2e402611624f03b6ac8
Marcial Rosales <mrosales@pivotal.io>
2017-08-30 12:23:00 +0200

    Add global exception handler. remove unnecessary file

--- load-flights-from-in-memory-db/FlightAvailability/FlightAvailability/Global.asax.cs
+++ load-flights-from-in-memory-db/FlightAvailability/FlightAvailability/Global.asax.cs
@@ -19,5 +19,11 @@ namespace FlightAvailability
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception lastError = Server.GetLastError();
+            Console.WriteLine("Unhandled exception: " + lastError.Message + lastError.StackTrace);
+        }
+
     }
 }
