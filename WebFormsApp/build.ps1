381a89676b0fe0d33d3152b4601694bab4b92c99
Marcial Rosales <mrosales@pivotal.io>
2017-11-08 23:14:50 +0100

    adding some log

--- WebFormsApp/build.ps1
+++ WebFormsApp/build.ps1
@@ -215,6 +215,7 @@ function Build-Solution($configuration) {
             $frameworkParam = "/p`:FrameworkPathOverride=`"$frameworkPath`""
         }
 
+        Write-Host "Listing folder $PublisherPath" -ForegroundColor Green
         Invoke-Expression "dir $PublisherPath"
 
         $app = "& $MsBuildApp /m /v:normal /p:Platform=$Architecture /p:Configuration=$configuration /nr:false $publish $tools $frameworkParam"
