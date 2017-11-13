0e279e19e8cda4e4f38704c8adfabea80d9982e6
Marcial Rosales <marcialfrg@hotmail.com>
2017-11-13 16:46:54 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-choco install microsoft-build-tools
+choco install microsoft-build-tools -y
 choco install nuget.commandline -y
 
 get-command nuget.exe
