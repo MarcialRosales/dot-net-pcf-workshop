236b9c0d79d7cba2c527fa699efa269ac700eaa7
Marcial Rosales <marcialfrg@hotmail.com>
2017-11-13 18:51:26 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,9 @@ Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-E
 
 choco install nuget.commandline -y
 
+Write-Host "listing chocolatey packages" -ForegroundColor Green
 choco list -li
 
+Write-Host "getting where nuget.exe" -ForegroundColor Green
 get-command NuGet.exe
 
