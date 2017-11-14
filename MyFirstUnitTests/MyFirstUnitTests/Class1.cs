e0184b0a8cc7ed3b329047efa653909cbd340003
Marcial Rosales <mrosales@pivotal.io>
2017-11-14 10:15:38 +0100

    add first library with unit tests

---
+++ MyFirstUnitTests/MyFirstUnitTests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(4, Add(3, 2));
+        }
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
---
+++ MyFirstUnitTests/build.ps1
@@ -0,0 +1,144 @@
+
+# These are the settings that could be messed with, but shouldn't be.
+Set-Variable -Option Constant -Name DefaultFrameworkPath -Value "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"
+Set-Variable -Option Constant -Name MsBuildApp           -Value "$DefaultFrameworkPath\MSBuild.exe"
+
+Set-Variable -Option Constant -Name XUnitVersion         -Value 2.3.1
+Set-Variable -Option Constant -Name XUnitApp             -Value ".\packages\xunit.runner.console.$XUnitVersion\tools\net452\xunit.console.exe"
+Set-Variable -Option Constant -Name NugetPath            -Value .\.nuget\nuget.exe
+Set-Variable -Option Constant -Name Architecture         -Value "'Any CPU'"
+
+
+# Write a nice banner for the peoples.
+function Write-Banner {
+    . {
+        Clear-Host
+        Write-Host "
+                                                              
+     ______       _ _     _  ______                            
+     | ___ \     (_) |   | | | ___ \                           
+     | |_/ /_   _ _| | __| | | |_/ / __ ___   ___ ___  ___ ___ 
+     | ___ \ | | | | |/ _`` | |  __/ '__/ _ \ / __/ _ \/ __/ __|
+     | |_/ / |_| | | | (_| | | |  | | | (_) | (_|  __/\__ \__ \
+     \____/ \__,_|_|_|\__,_| \_|  |_|  \___/ \___\___||___/___/`n" -ForegroundColor Gray
+
+        Write-Host " 
+               .--------.
+              / .------. \
+             / /        \ \
+             | |        | |
+            _| |________| |_
+          .' |_|        |_| '.
+          '._____ ____ _____.'
+          |     .'____'.     |
+          '.__.'.'    '.'.__.'
+          '.__  | LDAP |  __.'
+          |   '.'.____.'.'   |
+          '.____'.____.'____.'
+          '.________________.'" -ForegroundColor Green
+
+          Detect-Frameworks
+
+    } | Out-Null
+}
+
+
+# Detect the frameworks.
+function Detect-Frameworks {
+	$frameworks = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
+				Get-ItemProperty -name Version,Release,InstallPath -EA 0 
+
+}
+
+# Handles the installation of NuGet packages
+function NuGet-Install($package, $version) {
+    . {
+        Write-Host "Installing Nuget package $package, $version" -ForegroundColor Green
+            Invoke-Expression "$NugetPath install $package -outputdirectory .\packages -version $version" | Write-Host
+        Write-Host "Package installed successfully" -ForegroundColor Green
+    } | Out-Null
+}
+
+function NuGet-Restore() {
+    . {
+        Write-Host "Restoring Nuget package dependencies" -ForegroundColor Green
+            Invoke-Expression "$NugetPath restore /PackagesDirectory  ..\packages " | Write-Host
+        Write-Host "Packages installed successfully" -ForegroundColor Green
+    } | Out-Null
+}
+
+
+# Build the solution, return 
+function Build-Solution($configuration) {
+
+    $code = -1
+    . {
+        
+        NuGet-Restore
+
+
+        $app = "$MsBuildApp /m /v:normal  /nr:false "
+        Write-Host "Running the build script: $app" -ForegroundColor Green
+        Invoke-Expression "$app" | Write-Host
+        $code = $LastExitCode
+    } | Out-Null
+
+    if($code -ne 0) {
+        Write-Host "Build FAILED." -ForegroundColor Red
+    }
+    else{
+        Write-Host "Build SUCCESS." -ForegroundColor Green
+    }
+
+    $code
+}
+
+# The main entry point for this application.
+function main {
+    Write-Banner
+
+    $buildConfig = if ($(Get-Mode) -eq 'test') {'Test'} Else {'Release'}
+    $buildResult = Build-Solution $buildConfig
+    
+    if($buildResult -ne 0) {
+        Write-Host "Build failed, aborting..." -ForegroundColor Red
+        Exit $buildResult
+    } 
+
+    if($(Get-Mode) -eq 'test') {
+        Write-Host "Starting unit test execution" -ForegroundColor Green
+        NuGet-Install 'xunit.runner.console' $XUnitVersion
+        
+        $failedUnitTests = 0
+        
+        #Get the matching test assemblies, ensure only bin and the target architecture are selected
+        $testfiles = Get-ChildItem . -recurse  | where {$_.BaseName.EndsWith("Tests") -and $_.Extension -eq ".dll" `
+            -and $_.FullName -match "\\bin\\" -and $_.FullName -match "$Architecture"  }
+
+        #Execute unit tests in all assemblies, continue even in case of error, as it provides more context
+        foreach($UnitTestDll in $testfiles) {
+            Write-Host "Found Test: $($UnitTestDll.FullName)" -ForegroundColor Yellow
+            Invoke-Expression "$XUnitApp $($UnitTestDll.FullName)"
+
+            if( $LastExitCode -ne 0){
+                $failedUnitTests++
+                Write-Host "One or more tests in assembly FAILED" -ForegroundColor Red
+            } 
+            else
+            {
+                Write-Host "All tests in assembly passed" -ForegroundColor Green
+            }
+        }
+
+        if($failedUnitTests -ne 0) {
+            Write-Host "Unit testing for $(Get-Mode) configuration FAILED." -ForegroundColor Red
+            
+        } else {
+            Write-Host "Unit testing for $(Get-Mode) configuration completed successfully." -ForegroundColor Green
+        }
+        Exit $failedUnitTests
+    } 
+
+}
+
+main # Run the application.ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼￿￿￿￿￿￿￿￿￿￿￿￿﷽�﷽�ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼ﳼ￼
