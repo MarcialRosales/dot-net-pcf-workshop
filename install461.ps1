023e1523e170e7c8c33c6d797a28d25d853e4b4f
Marcial Rosales <mrosales@pivotal.io>
2017-11-15 10:11:13 +0100

    fix script

--- install461.ps1
+++ install461.ps1
@@ -14,7 +14,7 @@ Write-Host "`nCreating assemblies folder ...`n" -ForegroundColor Gray
 New-Item -ItemType Directory -path "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework"
 
 Write-Host "`nExtracting assemblies  ...`n" -ForegroundColor Gray
-Unzip "assemblies\v.4.6.1.zip" "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework"
+Unzip "assemblies\v-4.6.1.zip" "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework"
 
 Write-Host "`nListing .net framework assemblies  ...`n" -ForegroundColor Gray
 dir "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework"
