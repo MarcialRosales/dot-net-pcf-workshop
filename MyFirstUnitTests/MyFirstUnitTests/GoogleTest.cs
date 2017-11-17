a466fad36834b39d5708bb87bdccdbca14007504
Marcial Rosales <mrosales@pivotal.io>
2017-11-17 17:19:12 +0100

    added selenimum test

---
+++ MyFirstUnitTests/MyFirstUnitTests/GoogleTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using FluentAssertions;
+
+namespace MyFirstUnitTests
+{
+    public class GoogleTest
+    {
+        [Fact]
+        public void search()
+        {
+            ChromeDriver driver = null;
+
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://google.com");
+                IWebElement searchBox = driver.FindElementByCssSelector("input[type='text']");
+                searchBox.Should().NotBeNull();
+
+                driver.Close();
+                driver.Dispose();
+            }
+            catch
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
